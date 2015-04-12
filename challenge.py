2f31cc58e58c7b61e89b733d33fd6bcbeb3ac0a6
Chris Moultrie <cmoultrie@pindropsecurity.com>
2015-04-12 13:30:47 -0400

    First pass using itertools

--- challenge.py
+++ challenge.py
@@ -2,28 +2,35 @@
 
 import sys
 from collections import namedtuple
+from itertools import combinations_with_replacement
 
 Request = namedtuple('Request', ['operands', 'result'])
+OPERATORS = ['+', '-', '*', '/']
 
 
 class Challenge(object):
+
     def __init__(self):
         pass
 
     def run(self, str_input):
         self.process_input(str_input)
-        if self.is_too_small():
-            return 'Invalid'
-        else:
-            raise NotImplementedError
+        return self.try_combinations()
 
     def process_input(self, str_input):
         inputs = str_input.split(' ')
-        operands = [int(n) for n in inputs[:-1]]
-        self.request = Request(operands=operands, result=int(inputs[-1]))
+        self.request = Request(operands=inputs[:-1], result=int(inputs[-1]))
 
-    def is_too_small(self):
-        return False
+    def try_combinations(self):
+        combos = combinations_with_replacement(OPERATORS,
+                                               len(self.request.operands) - 1)
+        for combo in combos:
+            combo_str = ''.join(combo) + ' '
+            groups = list(zip(self.request.operands, combo_str))
+            seq = ' '.join([' '.join(group) for group in groups]).strip()
+            if eval(seq) == self.request.result:
+                return seq
+        return 'Invalid'
 
 
 if __name__ == '__main__':
--- tests/test_challenge.py
+++ tests/test_challenge.py
@@ -10,18 +10,18 @@ class TestChallenge():
     def test_process_input(self):
         """Handles the input"""
         self.challenge.process_input('1 2 3 5')
-        expected = Request(operands=[1, 2, 3], result=5)
+        expected = Request(operands=['1', '2', '3'], result=5)
         assert_equals(expected, self.challenge.request)
 
     def test_example_three_num_one(self):
         """Handles '1 2 3 5'"""
         result = self.challenge.run('1 2 3 5')
-        assert_equals(result, '3 + 2 * 1')
+        assert_equals(eval(result), eval('3 + 2 * 1'))
 
     def test_example_three_num_two(self):
         """Handles '4 6 3 2'"""
         result = self.challenge.run('4 6 3 2')
-        assert_equals(result, '4 / 6 * 3')
+        assert_equals(eval(result), eval('4 / 6 * 3'))
 
     def test_example_three_num_three(self):
         """Handles '1 1 1 6'"""
@@ -31,9 +31,9 @@ class TestChallenge():
     def test_example_five_num_one(self):
         """Handles '6 7 1 2 5 8'"""
         result = self.challenge.run('6 7 1 2 5 8')
-        assert_equals(result, '6 * 7 * 1 - 2 / 5')
+        assert_equals(eval(result), eval('6 * 7 * 1 - 2 / 5'))
 
     def test_example_five_num_two(self):
         """Handles '1 2 3 4 5 6 3'"""
         result = self.challenge.run('1 2 3 4 5 6 3')
-        assert_equals(result, '1 + 2 + 3 - 4 - 5 + 6')
+        assert_equals(eval(result), eval('1 + 2 + 3 - 4 - 5 + 6'))
